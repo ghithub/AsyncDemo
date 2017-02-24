6ca8b75c2f4baf0c1c0b9264cc4ab78c49dff51b
Zack <gnewsgroup@gmail.com>
2017-02-23 21:51:34 -0500

    Demonstrate how async and await works in C#

--- AsyncDemo/Program.cs
+++ AsyncDemo/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AsyncDemo
@@ -10,6 +11,41 @@ namespace AsyncDemo
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Starting");
+
+            var worker = new Worker();
+            worker.DoWork();
+
+            while(!worker.IsComplete)
+            {
+                Console.Write(".");
+                Thread.Sleep(100);
+            }
+
+            Console.WriteLine("Done");
+            Console.ReadKey();
+        }
+    }
+
+    public class Worker
+    {
+        public bool IsComplete { get; private set; }
+
+        public async void DoWork()
+        {
+            this.IsComplete = false;
+            Console.WriteLine("Doing work");
+            await LongOperation();
+            Console.WriteLine("\nWork completed");
+            IsComplete = true;
+        }
+
+        private Task LongOperation()
+        {
+            return Task.Factory.StartNew(()=> {
+                Console.WriteLine("Working!");
+                Thread.Sleep(2000);
+            });            
         }
     }
 }
